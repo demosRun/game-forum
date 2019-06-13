e97dfbf9c93656499e6d6918181cfec541d020a9
puge <mail@puge.org>
2019-06-13 15:11:43 +0800

    最终

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,27 @@ $(function () {
     paginationClickable: true,
     autoplay: 3000
   })
+
+  $('.swiper-zBLJxURsOhhagFAH ul').roundabout({
+    easing: 'easeOutInCirc',
+    duration: 600,
+    btnNext: ".next",
+    btnPrev: ".prev"
+  })
+
+  var swiper = new Swiper('.swiper-container-gvO4afIYFIhURa72', {
+    pagination: '.pagination-gvO4afIYFIhURa72',
+    autoplay: true,
+    loop: true,
+    paginationClickable: true,
+    autoplay: 3000
+  })
+
+  $('.prev-gvO4afIYFIhURa72').click(function(){
+    swiper.swipePrev()
+  })
+
+  $('.next-gvO4afIYFIhURa72').click(function(){
+    swiper.swipeNext()
+  })
 })
